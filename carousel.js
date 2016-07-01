e539da0c47634501d76000f85759b6903a2c213d
ylemberg <ylemberg7@yahoo.com>
2016-06-30 18:20:13 -0700

    Common functionality into separate files

---
+++ carousel.js
@@ -0,0 +1,50 @@
+function createCrsl(cnr) {
+    var crsl = document.createElement("div");
+    crsl.setAttribute('id', "myCarousel");
+    crsl.setAttribute('class', "carousel slide");
+    crsl.setAttribute('data-interval', "false");
+    crsl.setAttribute('keyboard', "true");
+    cnr.appendChild(crsl);
+
+    var crslInner = document.createElement("div");
+    crslInner.setAttribute('class', "carousel-inner");
+    crslInner.setAttribute('role', "listbox");
+    crslInner.setAttribute('id', 'crsl');
+    crsl.appendChild(crslInner);
+
+    return crsl;
+}
+
+function createLarrow(crsl) {
+    var left = document.createElement("a");
+    left.setAttribute('class', "left carousel-control");
+    left.setAttribute('href', "#myCarousel");
+    left.setAttribute('role', "button");
+    left.setAttribute('data-slide', "prev");
+    var lSpan1 = document.createElement("span");
+    lSpan1.setAttribute('class', "glyphicon glyphicon-chevron-left");
+    lSpan1.setAttribute('aria-hidden', "true");
+    var lSpan2 = document.createElement("span");
+    lSpan2.setAttribute('class', "sr-only");
+    lSpan2.innerHTML = "Previous";
+    left.appendChild(lSpan1);
+    left.appendChild(lSpan2);
+    crsl.appendChild(left);
+}
+
+function createRarrow(crsl) {
+    var right = document.createElement("a");
+    right.setAttribute('class', "right carousel-control");
+    right.setAttribute('href', "#myCarousel");
+    right.setAttribute('role', "button");
+    right.setAttribute('data-slide', "next");
+    var rSpan1 = document.createElement("span");
+    rSpan1.setAttribute('class', "glyphicon glyphicon-chevron-right");
+    rSpan1.setAttribute('aria-hidden', "true");
+    var rSpan2 = document.createElement("span");
+    rSpan2.setAttribute('class', "sr-only");
+    rSpan2.innerHTML = "Next";
+    right.appendChild(rSpan1);
+    right.appendChild(rSpan2);
+    crsl.appendChild(right);
+}
---
+++ songtheme.js
@@ -0,0 +1,80 @@
+function addCaptions(item, song) {
+    var caption = document.createElement('div');
+    caption.setAttribute('class', "carousel-caption");
+    var songTitle = document.createElement('h3');
+    songTitle.innerHTML = song.caption.substring(0, song.caption.indexOf(','));
+    var artist = document.createElement('p');
+    artist.innerHTML = song.caption.substring(song.caption.indexOf(',') + 1, song.caption.length);
+
+    caption.appendChild(songTitle);
+    caption.appendChild(artist);
+    item.appendChild(caption);
+}
+
+function addImages(item, song) {
+    var image = document.createElement('img');
+    image.setAttribute('src', "albumcovers/" + song.name + '/' + song.albumcover);
+    item.appendChild(image);
+}
+
+function addSongs(item, song) {
+    var sm2 = document.createElement('div');
+    sm2.setAttribute('class', "sm2-inline-list");
+    var ui360 = document.createElement('div');
+    ui360.setAttribute('class', "ui360 ui360-vis");
+    ui360.setAttribute('style', "margin: 65px 340px; position:absolute; top:0; left:0;");
+    var a = document.createElement('a');
+    a.setAttribute('href', "mp3s/" + song.name + '/' + song.mp3 + ".mp3");
+
+    ui360.appendChild(a);
+    sm2.appendChild(ui360);
+    item.appendChild(sm2);
+}
+
+function addWrappers(theme) {
+    if (data.initialCrsl) {
+        soundManager.setup({
+            url: 'musicplayer/soundmanager/swf/'
+        });
+        listThemeBtns(theme, data.initialCrsl);
+
+        var cntr = document.getElementsByClassName("container")[0];
+        var carousel = createCrsl(cntr);
+        createLarrow(carousel);
+        createRarrow(carousel);
+
+        for (var iDx = 0; iDx < theme.length; iDx++) {
+            var itemDiv = document.createElement('div');
+            if (iDx === 0) {
+                itemDiv.setAttribute('class', 'item active');
+            } else {
+                itemDiv.setAttribute('class', 'item');
+            }
+            carousel.childNodes[0].appendChild(itemDiv);
+            addImages(itemDiv, theme[iDx]);
+            addCaptions(itemDiv, theme[iDx]);
+            addSongs(itemDiv, theme[iDx]);
+        }
+        data.initialCrsl = false;
+    } else {
+        soundManager.reboot();
+        listThemeBtns(theme, data.initialCrsl);
+        var carousel = document.getElementById("crsl");
+        while (carousel.firstChild) {
+            carousel.removeChild(carousel.firstChild);
+        }
+
+        for (var iDx = 0; iDx < theme.length; iDx++) {
+            var itemDiv = document.createElement('div');
+            if (iDx === 0) {
+                itemDiv.setAttribute('class', 'item active');
+            } else {
+                itemDiv.setAttribute('class', 'item');
+            }
+            carousel.appendChild(itemDiv);
+            addImages(itemDiv, theme[iDx]);
+            addCaptions(itemDiv, theme[iDx]);
+            addSongs(itemDiv, theme[iDx]);
+        }
+    }
+}
